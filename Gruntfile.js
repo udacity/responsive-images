c49872f069f42333fe89d495b1cc1c2f34b488f2
Sam Dutton <dutton@google.com>
2014-12-11 16:10:12 -0800

    Gruntfile.js working for responsive images

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,31 @@
 module.exports = function(grunt) {
 
-grunt.initConfig({
-  responsive_images: {
-    dev: {
-      options: {
-        engine: "im"
-      },
-      files: [{
-        expand: true,
-        src: ['img/**/*.{gif,jpg,png}'],
-        cwd: 'src/',
-        dest: 'dist/'
-      }]
-    }
-  },
-});
+  grunt.initConfig({
+    responsive_images: {
+      dev: {
+        options: {
+          engine: 'im',
+          sizes: [{
+            name: 'small',
+            width: '30%',
+            suffix: '_20q_30pcw',
+            quality: 20
+          },{
+            name: 'large',
+            width: '50%',
+            suffix: '_40q_50pcw',
+            quality: 40
+          }]
+        },
+        files: [{
+          expand: true,
+          src: ['jpg/*.{gif,jpg,png}'],
+          cwd: 'images/',
+          dest: 'images/'
+        }]
+      }
+    },
+  });
 
   grunt.loadNpmTasks('grunt-responsive-images');
   grunt.registerTask('default', ['responsive_images']);
