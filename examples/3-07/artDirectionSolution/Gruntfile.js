e519b6e43f79f167ec8119910d005739dfee8349
cameronwp <cameron.w.pittman@gmail.com>
2015-02-18 13:58:51 -0800

    added 3-07 and prepped 3-03

---
+++ examples/3-07/artDirectionSolution/Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    responsive_images: {
+      dev: {
+        options: {
+          engine: 'im',
+          sizes: [{
+            width: 2400,
+            // suffix: '_large',
+            quality: 100
+          }]
+        },
+        files: [{
+          expand: true,
+          src: ['*.{gif,jpg,png}'],
+          cwd: 'images_src/',
+          dest: 'images/'
+        }]
+      }
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-responsive-images');
+  grunt.registerTask('default', ['responsive_images']);
+
+};
