ed1ce2146d00ecdb1c88113d4faefd9ba17e0286
cameronwp <cameron.w.pittman@gmail.com>
2015-03-06 11:39:27 -0800

    testing pagespeed for page bytes

--- project/project-grader.js
+++ project/project-grader.js
@@ -1,38 +1,97 @@
 var suites = [
   {
     name: "Lesson 2",
+    code: "ready!set!blog!",
+    tests: [
+      {
+        func: "testDOMelemCSS",
+        params: [
+          {
+            selector: "img",
+            property: "max-width",
+            value: "100%"
+          }
+        ],
+        desc: "&lt;img&gt;s have max-width of 100%"
+      },
+      {
+        func: "testDOMelemCSS",
+        params: [
+          {
+            selector: "article",
+            property: "width",
+            value: "600-1200px"
+          }
+        ],
+        desc: "&lt;articles&gt; are reasonably wide (600-1200px)" // descriptions must be unique
+      },
+      {
+        func: "testPageSize",
+        params: [
+          {
+            maxSize: 1500000 // in bytes
+          }
+        ],
+        desc: "Page bytes are under 1.5MB"
+      }
+    ]
+  },
+  {
+    name: "Lesson 3",
+    code: "myrelativesareskinny",
     tests: [
       {
         func: "testDOMelemCount",
         params: [
           {
             selector: "img",
-            count: 3
+            count: 9
           }
         ],
-        desc: "There are three images 1"
+        desc: "There are three images 3"
+      }
+    ]
+  },
+  {
+    name: "Lesson 4",
+    code: "myrelativesareaverage",
+    tests: [
+      {
+        func: "testDOMelemCount",
+        params: [
+          {
+            selector: "img",
+            count: 10
+          }
+        ],
+        desc: "There are three images 3"
       },
       {
         func: "testDOMelemCount",
         params: [
           {
             selector: "img",
-            count: 3
+            count: 10
           }
         ],
-        desc: "There are three images 2"
-      }
-    ]
-  },
-  {
-    name: "Lesson 3",
-    tests: [
+        desc: "There are three images 3"
+      },
       {
         func: "testDOMelemCount",
         params: [
           {
             selector: "img",
-            count: 3
+            count: 10
+          }
+        ],
+        desc: "There are three images 3"
+      },
+      {
+        func: "testDOMelemCount",
+        params: [
+          {
+            selector: "img",
+            count: 9
           }
         ],
         desc: "There are three images 3"
@@ -42,6 +101,5 @@ var suites = [
 ]
 
 var graderProperties = {
-  suites: suites,
-  code: "myrelativesarewide"
+  suites: suites
 }
