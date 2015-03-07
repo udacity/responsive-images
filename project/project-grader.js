0b52c681332f252a3d9bcc5151e3be40ef8efb2e
cameronwp <cameron.w.pittman@gmail.com>
2015-03-07 12:51:54 -0800

    added grader to all project index.htmls. added all mandatory tests for lesson 4 project

--- project/project-grader.js
+++ project/project-grader.js
@@ -7,12 +7,12 @@ var suites = [
         func: "testDOMelemCSS",
         params: [
           {
-            selector: "img",
+            selector: "article img",
             property: "max-width",
             value: "100%"
           }
         ],
-        desc: "&lt;img&gt;s have max-width of 100%"
+        desc: "&lt;img&gt;s have max-widths of 100%"
       },
       {
         func: "testDOMelemCSS",
@@ -50,13 +50,23 @@ var suites = [
         desc: "smiley_face.png is gone"
       },
       {
-        func: "testFindStringInHTML",
+        func: "testFindStringInDocument",
         params: [
           {
             stringOpts: ["☺", "&#9786;"] // looking for one of these
           }
         ],
         desc: "Smiley face is unicode (you may need to reload the page)"
+      },
+      {
+        func: "testMetaTagContent",
+        params: [
+          {
+            attr: "charset",
+            value: "utf-8"
+          }
+        ],
+        desc: "&lt;meta&gt; has charset set to utf-8"
       },
       {
         func: "testDOMelemDoesntExist",
@@ -66,6 +76,109 @@ var suites = [
           }
         ],
         desc: "Flourish is gone"
+      },
+      {
+        func: "testDOMelemAttrApproxContent",
+        params: [
+          {
+            selector: "*",
+            attrs: ["class"],
+            values: ["twitter"]
+          }
+        ],
+        desc: "A Twitter icon font is on the page"
+      },
+      {
+        func: "testDOMelemAttrApproxContent",
+        params: [
+          {
+            selector: "*",
+            attrs: ["class"],
+            values: ["digg"]
+          }
+        ],
+        desc: "A Digg icon font is on the page"
+      },
+      {
+        func: "testDOMelemAttrApproxContent",
+        params: [
+          {
+            selector: "*",
+            attrs: ["class"],
+            values: ["facebook"]
+          }
+        ],
+        desc: "A Facebook icon font is on the page"
+      },
+      {
+        func: "testDOMelemAttrApproxContent",
+        params: [
+          {
+            selector: "*",
+            attrs: ["class"],
+            values: ["google"]
+          }
+        ],
+        desc: "A Google+ icon font is on the page"
+      }
+    ]
+  },
+  {
+    name: "Lesson 4",
+    code: "allpictures,allthetime",
+    tests: [
+      {
+        func: "testDOMelemCount",
+        params: [
+          {
+            selector: "picture",
+            count: 8
+          }
+        ],
+        desc: "There are 8 &lt;picture&gt;s on the page."
+      },
+      {
+        func: "testDOMelemsCounts",
+        params: [
+          {
+            parentSelector: "picture",
+            childSelector: "source",
+            count: 2
+          }
+        ],
+        desc: "There are 2 &lt;sources&gt;s per &lt;picture&gt;."
+      },
+      {
+        func: "testDOMelemsCounts",
+        params: [
+          {
+            parentSelector: "picture",
+            childSelector: "img",
+            count: 1
+          }
+        ],
+        desc: "There is 1 &lt;img&gt;s per &lt;picture&gt;."
+      },
+      {
+        func: "testDOMelemsChildPosition",
+        params: [
+          {
+            parentSelector: "picture",
+            childSelector: "img",
+            position: -1
+          }
+        ],
+        desc: "&lt;img&gt; is last child of all &lt;picture&gt;s"
+      },
+      {
+        func: "testDOMelemsAttrContent",
+        params: [
+          {
+            selector: "img",
+            attr: "alt"
+          }
+        ],
+        desc: "Every &lt;img&gt; has an alt attribute"
       }
     ]
   }
