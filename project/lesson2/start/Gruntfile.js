683e5d0f2fe98d699783ad5b5b64cb5d891969eb
Sarah Clark <sarahclark@google.com>
2015-03-12 18:20:33 -0700

    Added explanatory comment at top

--- project/lesson2/start/Gruntfile.js
+++ project/lesson2/start/Gruntfile.js
@@ -1,3 +1,10 @@
+/*
+ Call with one of these options:
+  "grunt" alone creates a new, completed images directory
+  "grunt clean" removes the images directory
+  "grunt responsive_images" re-processes images without removing the old ones
+*/
+
 module.exports = function(grunt) {
 
   grunt.initConfig({
