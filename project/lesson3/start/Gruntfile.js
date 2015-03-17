d4bf97a09a88397b597e0a9631cab22e92709751
sarahclark <sarahclark@google.com>
2015-03-17 14:04:39 -0700

    Verified correctness

--- project/lesson3/start/Gruntfile.js
+++ project/lesson3/start/Gruntfile.js
@@ -27,41 +27,41 @@ module.exports = function(grunt) {
         }]
       }
     },
+
+    /* Clear out the images directory if it exists */
+    clean: {
+      dev: {
+        src: ['images'],
+      },
+    },
+
+    /* Generate the images directory if it is missing */
+    mkdir: {
+      dev: {
+        options: {
+          create: ['images']
+        },
+      },
+    },
+
+    /* Copy the "fixed" images that don't go through processing into the images/directory */
+    copy: {
+      dev: {
+        files: [{
+          expand: true,
+          src: ['images_src/fixed/*.{gif,jpg,png}'],
+          dest: 'images/',
+          flatten: true,
+        }]
+      },
+    },
+
   });
 
-
-      /* Clear out the images directory if it exists */
-      clean: {
-        dev: {
-          src: ['images'],
-        },
-      },
-
-      /* Generate the images directory if it is missing */
-      mkdir: {
-        dev: {
-          options: {
-            create: ['images']
-          },
-        },
-      },
-
-      /* Copy the "fixed" images that don't go through processing into the images/directory */
-      copy: {
-        dev: {
-          files: [{
-            expand: true,
-            src: 'images_src/fixed/*.{gif,jpg,png}',
-            dest: 'images/'
-          }]
-        },
-      },
-    });
-
-    grunt.loadNpmTasks('grunt-responsive-images');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-mkdir');
-    grunt.registerTask('default', ['clean', 'mkdir', 'copy', 'responsive_images']);
+  grunt.loadNpmTasks('grunt-responsive-images');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-mkdir');
+  grunt.registerTask('default', ['clean', 'mkdir', 'copy', 'responsive_images']);
 
 };
