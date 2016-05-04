dea9b88edc677c06b9dec35f3f12ecb30ba3755b
cameronwp <cameron.w.pittman@gmail.com>
2016-05-03 19:18:49 -0700

    updates relative width quiz

---
+++ examples/1-08/imageRelativeWidth/tests.js
@@ -0,0 +1,75 @@
+(function(window) {
+  var tests = [];
+
+  var test1 = function() {
+    var isCorrect = false;
+
+    var elem1 = document.querySelector('#img1');
+    var rect = elem1.getBoundingClientRect();
+    var e1RightSide = rect.right;
+
+    var elem2 = document.querySelector('#img2');
+    var rect = elem2.getBoundingClientRect();
+    var e2LeftSide = rect.left;
+
+    if (e2LeftSide - e1RightSide === 10) {
+      isCorrect = true;
+    }
+
+    window.dispatchEvent(new CustomEvent('ud-btwn-1-2', {'detail': 'passed'}));
+    return isCorrect;
+  };
+
+  var test2 = function() {
+    var isCorrect = false;
+
+    var elem2 = document.querySelector('#img2');
+    var rect = elem2.getBoundingClientRect();
+    var e2RightSide = rect.right;
+
+    var elem3 = document.querySelector('#img3');
+    var rect = elem3.getBoundingClientRect();
+    var e3LeftSide = rect.left;
+
+    if (e3LeftSide - e2RightSide === 10) {
+      isCorrect = true;
+    }
+
+    window.dispatchEvent(new CustomEvent('ud-btwn-2-3', {'detail': 'passed'}));
+    return isCorrect;
+  };
+
+  tests.push(test1);
+  tests.push(test2);
+
+  var isCorrect = false;
+
+  function executeTests() {
+    var isCorrect = false;
+
+    tests.forEach(function(test, index) {
+      var testCorrect = test();
+      if (index === 0) {
+        isCorrect = testCorrect;
+      } else {
+        isCorrect = isCorrect && testCorrect;
+      }
+      if (testCorrect) {
+        // to prevent unnecessary event firings
+        test = function() {};
+      }
+    });
+
+    return isCorrect;
+  }
+
+  isCorrect = executeTests();
+
+  var interval = window.setInterval(function() {
+    if (!isCorrect) {
+      isCorrect = executeTests();
+    } else {
+      window.clearInterval(interval);
+    }
+  }, 1000);
+})(window);
